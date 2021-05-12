9667008fbe8b50d66c1431067cc1e124831ece81
LuisMiguelRG <luis.miguel.dj22@gmail.com>
2021-05-11 20:28:41 -0500

    segundo

--- src/App.js
+++ src/App.js
@@ -17,9 +17,11 @@ function App() {
         >
           Learn React
         </a>
+      </header> 
+      <div>
         <h1>Mi primera aplicacion</h1>
         <p>Esta es mi primera aplicación en React, está padrísimo!</p>
-      </header>      
+      </div>     
     </div>
   );
 }
